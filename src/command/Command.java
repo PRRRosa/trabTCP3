40bbe447043484e909f13e46daaacfca50de0c4a
lain <arthur.zachow@gmail.com>
2018-06-18 22:10:18 -0300

    notas attr

--- src/command/Command.java
+++ src/command/Command.java
@@ -14,11 +14,19 @@ public abstract class Command implements UIAction {
 		this.systemOperationsImpl = (SystemOperationsImpl)systemOperation;
 		this.userTextInteraction = userTextInteraction;
 	}
+	
 	protected Command(SystemOperations systemOperation) {
 		this.systemOperationsImpl = (SystemOperationsImpl)systemOperation;
 	}
 
+	protected UserTextInteraction getUserTextInteraction() {
+		return this.userTextInteraction;
+	}
 
+	protected SystemOperationsImpl getSystemOperationsImpl() {
+		return this.systemOperationsImpl;
+	}
+	
 	/**
 	 * @see Command.UIAction#execute()
 	 */
